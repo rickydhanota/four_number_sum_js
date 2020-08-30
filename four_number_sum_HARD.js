206ff44c3ab88759371a566da9261189992eca17
Ricky <rickdhanota@gmail.com>
2020-08-30 00:51:59 -0700

    annoted and changed a few lines

--- four_number_sum_HARD.js
+++ four_number_sum_HARD.js
@@ -2,22 +2,22 @@
 
 function fourNumberSum(arr, targetSum) {
     // Write your code here.
-      var allPairSums = {};
-      var quadruplets = [];
-      for (var i=1; i<arr.length-1; i++){
-          for(var j = i+1; j<arr.length; j++){
-              var currentSum = arr[i] + arr[j];
-              var difference = targetSum - currentSum;
-              if (difference in allPairSums){
-                  for(var pair of allPairSums[difference]){
-                      quadruplets.push(pair.concat([arr[i], arr[j]]));
-                    }
+    var allPairSums = {};
+    var quadruplets = [];
+    for (var i=1; i<arr.length-1; i++){
+        for(var j = i+1; j<arr.length; j++){//Looking at the next number from i=0
+            var currentSum = arr[i] + arr[j]; //Sum up 
+            var difference = targetSum - currentSum; //Subtract by the target to find the difference
+            if (difference in allPairSums){//Checking to see if the difference exists in the allPairSums dictionary, and on the first pass it wont exist because there should be nothing in there
+                for(var pair of allPairSums[difference]){
+                    quadruplets.push(pair.concat( [arr[i], arr[j]] ) );
                 }
             }
+        }
         for (let k=0; k<i; k++){
             var currentSum = arr[i] + arr[k];
             if(!(currentSum in allPairSums)){
-                allPairSums[currentSum] = [[arr[k], arr[i]]];
+                allPairSums[currentSum] = [ [arr[k], arr[i]] ];
             }
             else{
                 allPairSums[currentSum].push([arr[k], arr[i]]);
