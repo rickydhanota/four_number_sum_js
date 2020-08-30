d3ef00e07d70b929769db86db2c3d3d800ef0d7b
Ricky <rickdhanota@gmail.com>
2020-08-30 10:21:01 -0700

    more annotations

--- four_number_sum_HARD.js
+++ four_number_sum_HARD.js
@@ -2,25 +2,26 @@
 
 function fourNumberSum(arr, targetSum) {
     // Write your code here.
-    var allPairSums = {};
+    var allPairSums = {}; //Creating an object
     var quadruplets = [];
-    for (var i=1; i<arr.length-1; i++){
-        for(var j = i+1; j<arr.length; j++){//Looking at the next number from i=0
+    for (var i=1; i<arr.length-1; i++){// we can start at the second value because at the first value theres nothing in our javascript object, and we can skip the last value because there is nothing that comes after the last value
+        for(var j = i+1; j<arr.length; j++){//Looking at the next number from i
             var currentSum = arr[i] + arr[j]; //Sum up 
             var difference = targetSum - currentSum; //Subtract by the target to find the difference
             if (difference in allPairSums){//Checking to see if the difference exists in the allPairSums dictionary, and on the first pass it wont exist because there should be nothing in there
                 for(var pair of allPairSums[difference]){
-                    quadruplets.push(pair.concat( [arr[i], arr[j]] ) );
+                    quadruplets.push(pair.concat( [arr[i], arr[j]] ) );//.concat merges 2 arrays
                 }
             }
         }
         for (let k=0; k<i; k++){
             var currentSum = arr[i] + arr[k];
             if(!(currentSum in allPairSums)){
-                allPairSums[currentSum] = [ [arr[k], arr[i]] ];
+                allPairSums[currentSum] = [ [arr[k], arr[i]] ]; //Creates a key value pair, by first creating the key "currentSum" and assigning it a value of [ [arr[k], arr[i]] ]
             }
             else{
                 allPairSums[currentSum].push([arr[k], arr[i]]);
+                // console.log("all pairs", allPairSums); if we have duplicates it pushes
             }
         }
     }
