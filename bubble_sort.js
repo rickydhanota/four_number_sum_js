677c5e5a623f7059368f819f5329fef8ef3082fa
Ricky <rickdhanota@gmail.com>
2020-08-30 13:56:36 -0700

    added more notes

---
+++ bubble_sort.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr){
+    for(var i=0; i<arr.length; i++){
+        for(var j=0; j<arr.length;j++){
+            if(arr[j]>arr[j+1]){
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+console.log(bubbleSort([1,4,2,3,6,7,2,1]));
