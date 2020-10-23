34050fd7bd855170b9bdb4b50ccb92a5d0f93f60
Ricky <rickdhanota@gmail.com>
2020-10-22 23:10:15 -0700

    adding the logic for the methods

--- Heaps/min_heap_contruction.py
+++ Heaps/min_heap_contruction.py
@@ -26,19 +26,45 @@ class MinHeap:
         self.heap = self.buildHeap(array)
     
     def buildHeap(self, array):
-        pass
+        firstParentIdx = (len(array) - 2) // 2 #floor devision, it just rounds down
+        for currentIdx in reversed(range(firstParentIdx + 1)):
+            self.siftDown(currentIdx, len(array) - 1, array)
+        return array
 
-    def siftdown(self):
-        pass
+    def siftDown(self, currentIdx, endIdx, heap):
+        childOneIdx = currentIdx * 2 + 1
+        while childOneIdx <= endIdx:
+            childTwoIdx = currentIdx * 2 + 2 if currentIdx * 2 + 2 <= endIdx else -1
+            if childTwoIdx != -1 and heap[childTwoIdx] < heap[childOneIdx]:
+                idxToSwap = childTwoIdx
+            else:
+                idxToSwap = childOneIdx
+            if heap[idxToSwap] < heap[currentIdx]:
+                self.swap(currentIdx, idxToSwap, heap)
+                currentIdx = idxToSwap
+                childOneIdx = currentIdx * 2 + 1
+            else:
+                return
 
-    def siftup(self):
-        pass
+    def siftUp(self, currentIdx, heap):
+        parentIdx = (currentIdx - 1) // 2
+        while currentIdx > 0 and heap[currentIdx] < heap[parentIdx]:
+            self.swap(currentIdx, parentIdx, heap)
+            currentIdx = parentIdx
+            parentIdx = (currentIdx - 1) // 2
 
     def peek(self):
-        pass
+        return self.heap[0]
 
     def remove(self):
-        pass
+        self.swap(0, len(self.heap) - 1, self.heap)
+        valueToRemove = self.heap.pop()
+        self.siftDown(0, len(self.heap) - 1, self.heap)
+        return valueToRemove
 
     def insert(self, value):
-        pass
+        self.heap.append(value)
+        self.siftUp(len(self.heap) - 1, self.heap)
+    
+    def swap(self, i, j, heap):
+        heap[i], heap[j] = heap[j], heap[i]
